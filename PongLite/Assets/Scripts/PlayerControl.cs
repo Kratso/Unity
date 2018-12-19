f384a91f49efa767981de771a0b58de872463767
Kratso <kratsothelsamar@gmail.com>
2018-12-19 08:46:01 +0100

    unity algo cambiado?

---
+++ PongLite/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            speed = Input.GetAxisRaw("vertical") * Time.deltaTime;
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            speed = Input.GetAxisRaw("vertical") * Time.deltaTime;
+	}
+}
