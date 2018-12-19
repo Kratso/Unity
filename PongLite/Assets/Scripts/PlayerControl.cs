152f445c2e6baea2bbf3006e6c5eff8957908f44
Kratso <kratsothelsamar@gmail.com>
2018-12-19 09:50:27 +0100

    asd

--- PongLite/Assets/Scripts/PlayerControl.cs
+++ PongLite/Assets/Scripts/PlayerControl.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
-    public float speed;
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            speed = Input.GetAxisRaw("vertical") * Time.deltaTime;
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-            speed = Input.GetAxisRaw("vertical") * Time.deltaTime;
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            float y_ax = speed * Input.GetAxisRaw("Vertical") * Time.deltaTime;
+            Vector3 mov = new Vector3(0, y_ax, 0);
+            if ((transform.position.y < 3.85 && y_ax > 0) || (transform.position.y>-1.96 && y_ax < 0))
+                transform.position += mov;
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position.Set(transform.position.x,0.66f,transform.position.z);
+    }
 }
